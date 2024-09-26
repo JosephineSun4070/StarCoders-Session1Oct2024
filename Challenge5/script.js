cb7d3e40b38e0c8dd4ad1ebbc594c57458f73e56
Rachel Lee <59715635+rlee9109@users.noreply.github.com>
2024-09-26 17:58:14 +0000

    Added challenges

---
+++ Challenge5/script.js
@@ -0,0 +1,148 @@
+var data = {
+	transactionData: [
+		{
+			submissionDate: '07/08/2020',
+			transactionId: '736652570090',
+			amount: 7800000.00,
+			currency: 'USD',
+			companyCode: 'MGST',
+			userId: 'jdoe@company.com',
+			accountTypeCode: 'C',
+			comments: 'Email sent Sat 7/4',
+			status: 'P',
+			approvedDate: null,
+			companyTaxId: 'F6789JUK',
+			approvalUserId: 'ssmith@company.com'
+			
+		},
+		{
+			submissionDate: '09/21/2019',
+			transactionId: '7899901234123',
+			amount: 900000000.00,
+			currency: 'CNY',
+			companyCode: 'GHNT',
+			userId: 'jsmith@acme.org',
+			accountTypeCode: 'A',
+			comments: 'Approved 9/22 C Carlisle',
+			status: 'A',
+			approvedDate: '09/22/2019',
+			companyTaxId: 'F9988HUYY',
+			approvalUserId: 'ttucker@acme.com'
+			
+		},
+	
+			{
+			submissionDate: '10/31/2019',
+			transactionId: '876456333289',
+			amount: 41342000.98,
+			currency: 'CAD',
+			companyCode: 'HTST',
+			userId: 'hjohnson@test.com',
+			accountTypeCode: 'B',
+			comments: 'Delivery TBD',
+			status: 'P',
+			approvedDate: null,
+			companyTaxId: 'K8767KIPU',
+			approvalUserId: 'yrutherford@test.com'
+			
+		},
+	
+			{
+			submissionDate: '03/12/2020',
+			transactionId: '109888723567',
+			amount: 900876750.98,
+			currency: 'USD',
+			companyCode: 'MGST',
+			userId: 'fthomson@place.com',
+			accountTypeCode: 'C',
+			comments: 'See Trans. 901766855453',
+			status: 'A',
+			approvedDate: '03/14/2020',
+			companyTaxId: 'H8767YTTR',
+			approvalUserId: 'klee@place.com'
+			
+		},
+	
+			{
+			submissionDate: '08/16/2020',
+			transactionId: '988765409233',
+			amount: 800999788.25,
+			currency: 'USD',
+			companyCode: 'GGRF',
+			userId: 'ppatterson@billygoat.com',
+			accountTypeCode: 'B',
+			comments: 'Cross-Ref 876555434489',
+			status: 'P',
+			approvedDate: null,
+			companyTaxId: 'L1255PLOU',
+			approvalUserId: 'lkay@billygoat.com'
+			
+		},
+	
+			{
+			submissionDate: '02/01/2020',
+			transactionId: '765444837099',
+			amount: 178222.87,
+			currency: 'CAD',
+			companyCode: 'PPKT',
+			userId: 'rfrank@reverse.com',
+			accountTypeCode: 'A',
+			comments: 'First of the Month Payment',
+			status: 'A',
+			approvedDate: '02/01/2020',
+			companyTaxId: 'Q0155BGFR',
+			approvalUserId: 'rjones@reverse.com'
+			
+		},
+	
+			{
+			submissionDate: '04/15/2020',
+			transactionId: '762100987620',
+			amount: 768955111.00,
+			currency: 'USD',
+			companyCode: 'KJTF',
+			userId: 'lkind@splash.com',
+			accountTypeCode: 'C',
+			comments: 'Confirm receipt before approval',
+			status: 'P',
+			approvedDate: null,
+			companyTaxId: 'S7854YTRE',
+			approvalUserId: 'jemeno@splash.com'
+			
+		},
+	
+			{
+			submissionDate: '04/25/2020',
+			transactionId: '900234654888',
+			amount: 8000000.00,
+			currency: 'USD',
+			companyCode: 'ORTO',
+			userId: 'bphilips@orto.com',
+			accountTypeCode: 'A',
+			comments: 'Approved by K Peters',
+			status: 'A',
+			approvedDate: '04/27/2020',
+			companyTaxId: 'I9009FDSW',
+			approvalUserId: 'kpeters@orto.com'
+			
+		},
+	
+			{
+			submissionDate: '01/30/2020',
+			transactionId: '309878658999',
+			amount: 65540099.87,
+			currency: 'USD',
+			companyCode: 'LREW',
+			userId: 'sfryer@brick.com',
+			accountTypeCode: 'B',
+			comments: 'Invoice 78655',
+			status: 'P',
+			approvedDate: null,
+			companyTaxId: 'E4344LCXW',
+			approvalUserId: 'pcharles@brick.com'
+			
+		},
+	
+	
+	]
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+Session 1 Date: 
+Oct 23 @5pm-6:30
+
+Notes:
+- only owner can use the Live Preview extension
